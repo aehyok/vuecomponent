c82c15ca8c8486a4d688f4f5ffb6bdf70d488729
刘启明 <455043818@qq.com>
2021-02-05 12:01:11 +0800

    隐藏jsonEditor顶部菜单

--- src/views/componentManage/form.vue
+++ src/views/componentManage/form.vue
@@ -61,7 +61,7 @@ export default {
   },
   data() {
     return {
-      options: { mode: 'code' },
+      options: { mode: 'code', mainMenuBar: false },
       formConfig: {
         formListItem: [
           {
